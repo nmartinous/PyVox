9cfed34475ffc82ae07153793950b000a71e8a53
Nolan Martinous <91758940+nmartinous@users.noreply.github.com>
2024-03-07 20:19:31 -0500

    add camera and player scripts

--- settings.py
+++ settings.py
@@ -6,11 +6,10 @@ import numpy as np
 import glm
 import math
 
-
-# WINDOW RESOLUTION
-WIN_RES = glm.vec2(1600, 900)  # Set resolution to 1600x900
-
 # COLORS
 # -----------------------------------------------
 # Background color
 BG_COLOR = glm.vec3(0.1, 0.16, 0.25)
+
+# WINDOW RESOLUTION
+WIN_RES = glm.vec2(1600, 900)  # Set resolution to 1600x900
